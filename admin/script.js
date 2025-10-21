c2d2ee5c2eadc1e86caadd98d78038be5887d570
K <3124410154@sv.sgu.edu.vn>
2025-10-21 17:29:58 +0700

    section

--- admin/script.js
+++ admin/script.js
@@ -39,6 +39,26 @@ if (burgerBtn && sidebar) {
         sidebar.classList.toggle('open');
     });
 }
+const sidebars = document.querySelectorAll(".sidebar-list-item.tab-content");
+const sections = document.querySelectorAll(".section");
+
+for(let i = 0; i < sidebars.length; i++) {
+    sidebars[i].onclick = function () {
+        document.querySelector(".sidebar-list-item.active").classList.remove("active");
+        document.querySelector(".section.active").classList.remove("active");
+        sidebars[i].classList.add("active");
+        sections[i].classList.add("active");
+    };
+}
+
+const closeBtn = document.querySelectorAll('.section');
+console.log(closeBtn[0])
+for(let i=0;i<closeBtn.length;i++){
+    closeBtn[i].addEventListener('click',(e) => {
+        sidebar.classList.add("open");
+    })
+}
+
 
 // Xử lý nút Home page
 const homeBtn = document.querySelector('.bottom-sidebar .user-logout:nth-child(1) a');
