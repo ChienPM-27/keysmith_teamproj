2adcee2969275e23bebb9b914301b867f2538463
K <3124410154@sv.sgu.edu.vn>
2025-11-13 17:35:03 +0700

    HOT FIX

--- js/sampledata/sampleData.js
+++ js/sampledata/sampleData.js
@@ -399,19 +399,6 @@ export const sampleData = {
 			status: "active"
 		}
 	],
-	catagories: [
-		"Attack On Titan",
-		"LOTR",
-		"One Piece",
-		"YuGiOh",
-		"Cyberpunk Neon Series",
-		"Cosmic Odyssey",
-		"Mythic Creatures",
-		"Shadow Reaver Series",
-		"Zen Aesthetics",
-		"Elemental Frost",
-		"Aurora Dream",
-	],
 	orders: [
 		{
 			username: "cust01",
